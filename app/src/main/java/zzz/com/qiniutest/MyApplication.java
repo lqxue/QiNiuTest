bef521f000a679175c9eaf1543e646d10727b59e
user <gitolite@123.56.137.112:zcsj_app_android.git>
2017-03-22 15:38:54 +0800

    修改application

--- app/src/main/java/zzz/com/qiniutest/MyApplication.java
+++ app/src/main/java/zzz/com/qiniutest/MyApplication.java
@@ -8,7 +8,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //获取全局上下文
         mContext = getApplicationContext();
     }
 
